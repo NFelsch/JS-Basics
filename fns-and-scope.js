102166ed2ad5569fcdbf44b2c6ace285b3866ebe
Nicholas Felsch <nicholas.felsch@hotmail.com>
2017-05-06 09:34:05 -0600

    Update-1

--- fns-and-scope.js
+++ fns-and-scope.js
@@ -3,6 +3,12 @@
 var name = 'Tyler';
 //Create a function called isTyler that accepts name as it's only parameter.
 //If the argument you passed in is equal to 'Tyler', return true. If it's not, return false.
+var isTyler = function (name) {
+  if name === 'Tyler';
+  return true;
+} else {
+  return false;
+}
 
   //Code Here
 
